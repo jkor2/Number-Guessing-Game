7f0f3d1a1319b7aacb62bd01814089d6605be553
Jake Korobellis <jkorobellis@gmail.com>
2023-07-18 23:56:45 -0400

    Adding comments to explain logic

--- numberguess.cpp
+++ numberguess.cpp
@@ -15,30 +15,32 @@ int main()
     int random = (rand() % 10) + 1;
     // Inital guess
     int inital_guess;
-
     cin >> inital_guess;
 
+    // Determines if the guess was correct or not for the while loop
     bool guess_staus = 0;
 
+    // Handle inital guess
     if (inital_guess == random || guess_staus == 1)
     {
         cout << "Good Job, you are correct!" << endl;
         guess_staus = 1;
     }
-    else
+    else // If the guess is incorrect
     {
-
-        do
+        do // Start the do/while loop
         {
             cout << "Incorrect, try again! > " << flush;
             int guess_two;
             cin >> guess_two;
 
+            // if the following guesses are correct, change the guess status
             if (guess_two == random)
             {
                 guess_staus = 1;
             }
 
+            // correct guess status exit the loop
         } while (guess_staus == 0);
         {
             cout << "Good Job, you are correct!" << endl;
